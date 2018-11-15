e4b9c071adc674fc5d57bdf411a428057f475570
dairong <ambera23325a@gmail.com>
2018-11-15 16:12:46 +0800

    first commit

---
+++ MassSpringSystem/CMassSpringSystem.cpp
@@ -0,0 +1,458 @@
+#include <cmath>
+#include <iostream>
+#include "configFile.h"
+#include "CMassSpringSystem.h"
+#include "glut.h"
+#include "Render_API.h"
+
+#pragma comment( lib, "glut32.lib" )
+
+const double g_cdDeltaT = 0.001f;
+const double g_cdK	   = 2500.0f;
+const double g_cdD	   = 50.0f;
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Constructor & Destructor
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+CMassSpringSystem::CMassSpringSystem()
+   :m_bDrawParticle(true),
+    m_bDrawStruct(false),
+    m_bDrawShear(false),
+    m_bDrawBending(false),
+    m_bDrawGoalpost(true),
+    m_bSimulation(false),
+
+    m_iIntegratorType(EXPLICIT_EULER),
+
+    m_dDeltaT(g_cdDeltaT),
+    m_dSpringCoefStruct(g_cdK),
+    m_dSpringCoefShear(g_cdK),
+    m_dSpringCoefBending(g_cdK),
+    m_dDamperCoefStruct(g_cdD),
+    m_dDamperCoefShear(g_cdD),
+    m_dDamperCoefBending(g_cdD),
+
+    m_ForceField(Vector3d(0.0,-9.8,0.0)),
+
+    m_GoalNet(),
+    m_Balls()
+{
+}
+
+CMassSpringSystem::CMassSpringSystem(const std::string &a_rcsConfigFilename)
+:m_GoalNet(a_rcsConfigFilename)
+{
+    int iIntegratorType;
+    double dSpringCoef,dDamperCoef;
+
+    ConfigFile configFile;
+    configFile.suppressWarnings(1);
+
+    configFile.addOption("DrawParticle"        ,&m_bDrawParticle);
+    configFile.addOption("DrawSpringStructural",&m_bDrawStruct);
+    configFile.addOption("DrawSpringShear"     ,&m_bDrawShear);
+    configFile.addOption("DrawSpringBending"   ,&m_bDrawBending);
+    configFile.addOption("DrawGoalpost"        ,&m_bDrawGoalpost);
+    configFile.addOption("SimulationStart"     ,&m_bSimulation);
+
+    configFile.addOption("IntegratorType",&iIntegratorType);
+    configFile.addOption("DeltaT"    ,&m_dDeltaT);
+    configFile.addOption("SpringCoef",&dSpringCoef);
+    configFile.addOption("DamperCoef",&dDamperCoef);
+
+    int code = configFile.parseOptions((char *)a_rcsConfigFilename.c_str());
+    if(code == 1)
+    {
+        std::cout<<"Error in CMassSpringSystem constructor."<<std::endl;
+        system("pause");
+        exit(0);
+    }
+    m_iIntegratorType = CMassSpringSystem::EXPLICIT_EULER;
+    if(iIntegratorType == 1)
+    {
+        m_iIntegratorType = CMassSpringSystem::RUNGE_KUTTA;
+    }
+
+    m_dSpringCoefStruct  = dSpringCoef;
+    m_dSpringCoefShear   = dSpringCoef;
+    m_dSpringCoefBending = dSpringCoef;
+    m_dDamperCoefStruct  = dDamperCoef;
+    m_dDamperCoefShear   = dDamperCoef;
+    m_dDamperCoefBending = dDamperCoef;
+
+    m_ForceField   = Vector3d(0.0,-9.8,0.0);
+
+    Reset();
+}
+
+CMassSpringSystem::CMassSpringSystem(const CMassSpringSystem &a_rcMassSpringSystem)
+    :m_bDrawParticle(a_rcMassSpringSystem.m_bDrawParticle),
+    m_bDrawStruct(a_rcMassSpringSystem.m_bDrawStruct),
+    m_bDrawShear(a_rcMassSpringSystem.m_bDrawShear),
+    m_bDrawBending(a_rcMassSpringSystem.m_bDrawBending),
+    m_bDrawGoalpost(a_rcMassSpringSystem.m_bDrawGoalpost),
+    m_bSimulation(a_rcMassSpringSystem.m_bSimulation),
+
+    m_iIntegratorType(a_rcMassSpringSystem.m_iIntegratorType),
+
+    m_dDeltaT(a_rcMassSpringSystem.m_dDeltaT),
+    m_dSpringCoefStruct(a_rcMassSpringSystem.m_dSpringCoefStruct),
+    m_dSpringCoefShear(a_rcMassSpringSystem.m_dSpringCoefShear),
+    m_dSpringCoefBending(a_rcMassSpringSystem.m_dSpringCoefBending),
+    m_dDamperCoefStruct(a_rcMassSpringSystem.m_dDamperCoefStruct),
+    m_dDamperCoefShear(a_rcMassSpringSystem.m_dDamperCoefShear),
+    m_dDamperCoefBending(a_rcMassSpringSystem.m_dDamperCoefBending),
+
+    m_ForceField(a_rcMassSpringSystem.m_ForceField)
+{
+}
+CMassSpringSystem::~CMassSpringSystem()
+{
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Draw
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void CMassSpringSystem::Draw()
+{
+    DrawGoalNet();
+    DrawBall();
+}
+
+void CMassSpringSystem::DrawGoalNet()
+{    
+    // draw particle
+    if (m_bDrawParticle)
+    {
+        glPushAttrib(GL_CURRENT_BIT);
+        for (int uiI = 0; uiI < m_GoalNet.ParticleNum(); uiI++)
+        {
+            setColor3f(1.0, 0.0, 0.0);
+            drawPoint(m_GoalNet.GetParticle(uiI).GetPosition(), 3.0);
+        }
+        glPopAttrib();
+    }
+
+    // draw spring
+    glPushAttrib(GL_CURRENT_BIT);
+    for (int uiI = 0; uiI < m_GoalNet.SpringNum(); uiI++)
+    {
+        if ((m_GoalNet.GetSpring(uiI).GetSpringType() == CSpring::Type_nStruct && m_bDrawStruct) ||
+            (m_GoalNet.GetSpring(uiI).GetSpringType() == CSpring::Type_nShear && m_bDrawShear) ||
+            (m_GoalNet.GetSpring(uiI).GetSpringType() == CSpring::Type_nBending && m_bDrawBending))
+        {
+            int iSpringStartID = m_GoalNet.GetSpring(uiI).GetSpringStartID();
+            int iSpringEndID = m_GoalNet.GetSpring(uiI).GetSpringEndID();
+            Vector3d springColor = m_GoalNet.GetSpring(uiI).GetSpringColor();
+            Vector3d startPos = m_GoalNet.GetParticle(iSpringStartID).GetPosition();
+            Vector3d endPos = m_GoalNet.GetParticle(iSpringEndID).GetPosition();
+
+            setColor3fv(springColor);
+            drawLine(startPos, endPos);                   
+        }
+    }
+    glPopAttrib();
+
+    if (m_bDrawGoalpost)
+    {
+        DrawGoalpost();
+    }
+}
+
+void CMassSpringSystem::DrawGoalpost()
+{
+    // draw cylinder
+    int widthNum = m_GoalNet.GetWidthNum();
+    int heightNum = m_GoalNet.GetHeightNum();
+    int lengthNum = m_GoalNet.GetLengthNum();
+
+    int backBottomRightId = m_GoalNet.GetParticleID(0, 0, 0);
+    int backBottomLeftId = m_GoalNet.GetParticleID(0, 0, lengthNum - 1);
+    int frontBottomRightId = m_GoalNet.GetParticleID(widthNum - 1, 0, 0);
+    int frontBottomLeftId = m_GoalNet.GetParticleID(widthNum - 1, 0, lengthNum - 1);
+    int backTopRightId = m_GoalNet.GetParticleID(0, heightNum - 1, 0);
+    int backTopLeftId = m_GoalNet.GetParticleID(0, heightNum - 1, lengthNum - 1);
+    int frontTopRightId = m_GoalNet.GetParticleID(widthNum - 1, heightNum - 1, 0);
+    int frontTopLeftId = m_GoalNet.GetParticleID(widthNum - 1, heightNum - 1, lengthNum - 1);
+    drawCylinder(
+        m_GoalNet.GetParticle(backBottomLeftId).GetPosition(),
+        m_GoalNet.GetParticle(backTopLeftId).GetPosition(),
+        0.05);
+    drawCylinder(
+        m_GoalNet.GetParticle(backBottomRightId).GetPosition(),
+        m_GoalNet.GetParticle(backTopRightId).GetPosition(),
+        0.05);
+    drawCylinder(
+        m_GoalNet.GetParticle(backTopRightId).GetPosition(),
+        m_GoalNet.GetParticle(backTopLeftId).GetPosition(),
+        0.05);
+    drawCylinder(
+        m_GoalNet.GetParticle(backTopRightId).GetPosition(),
+        m_GoalNet.GetParticle(frontTopRightId).GetPosition(),
+        0.05);
+    drawCylinder(
+        m_GoalNet.GetParticle(backTopLeftId).GetPosition(),
+        m_GoalNet.GetParticle(frontTopLeftId).GetPosition(),
+        0.05);
+    drawCylinder(
+        m_GoalNet.GetParticle(frontTopRightId).GetPosition(),
+        m_GoalNet.GetParticle(frontTopLeftId).GetPosition(),
+        0.05);
+    drawCylinder(
+        m_GoalNet.GetParticle(frontBottomRightId).GetPosition(),
+        m_GoalNet.GetParticle(frontTopRightId).GetPosition(),
+        0.05);
+    drawCylinder(
+        m_GoalNet.GetParticle(frontBottomLeftId).GetPosition(),
+        m_GoalNet.GetParticle(frontTopLeftId).GetPosition(),
+        0.05);
+}
+
+void CMassSpringSystem::DrawBall()
+{
+    for (int ballIdx = 0; ballIdx < BallNum(); ++ballIdx)
+    {
+        drawSolidBall(m_Balls[ballIdx].GetPosition(), m_Balls[ballIdx].GetRadius());
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Set and Update
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void CMassSpringSystem::Reset()
+{ 
+    m_GoalNet.Reset();
+    m_Balls.clear();
+}
+
+void CMassSpringSystem::SetSpringCoef(const double a_cdSpringCoef, const CSpring::enType_t a_cSpringType)
+{
+    if (a_cSpringType == CSpring::Type_nStruct)
+    {
+        m_dSpringCoefStruct = a_cdSpringCoef;
+        m_GoalNet.SetSpringCoef(a_cdSpringCoef, CSpring::Type_nStruct);
+    }
+    else if (a_cSpringType == CSpring::Type_nShear)
+    {
+        m_dSpringCoefShear = a_cdSpringCoef;
+        m_GoalNet.SetSpringCoef(a_cdSpringCoef, CSpring::Type_nShear);
+    }
+    else if (a_cSpringType == CSpring::Type_nBending)
+    {
+        m_dSpringCoefBending = a_cdSpringCoef;
+        m_GoalNet.SetSpringCoef(a_cdSpringCoef, CSpring::Type_nBending);
+    }
+    else
+    {
+        std::cout << "Error spring type in CMassSpringSystme SetSpringCoef" << std::endl;
+    }
+
+}
+
+void CMassSpringSystem::SetDamperCoef(const double a_cdDamperCoef, const CSpring::enType_t a_cSpringType)
+{
+    if (a_cSpringType == CSpring::Type_nStruct)
+    {
+        m_dDamperCoefStruct = a_cdDamperCoef;
+        m_GoalNet.SetDamperCoef(a_cdDamperCoef, CSpring::Type_nStruct);
+    }
+    else if (a_cSpringType == CSpring::Type_nShear)
+    {
+        m_dDamperCoefShear = a_cdDamperCoef;
+        m_GoalNet.SetDamperCoef(a_cdDamperCoef, CSpring::Type_nShear);
+    }
+    else if (a_cSpringType == CSpring::Type_nBending)
+    {
+        m_dDamperCoefBending = a_cdDamperCoef;
+        m_GoalNet.SetDamperCoef(a_cdDamperCoef, CSpring::Type_nBending);
+    }
+    else
+    {
+        std::cout << "Error spring type in CMassSpringSystme SetDamperCoef" << std::endl;
+    }
+}
+
+double CMassSpringSystem::GetSpringCoef(const CSpring::enType_t a_cSpringType)
+{
+    if(a_cSpringType == CSpring::Type_nStruct)
+    {
+        return m_dSpringCoefStruct;
+    }
+    else if(a_cSpringType == CSpring::Type_nShear)
+    {
+        return m_dSpringCoefShear;
+    }
+    else if(a_cSpringType == CSpring::Type_nBending)
+    {
+        return m_dSpringCoefBending;
+    }
+    else
+    {
+        std::cout<<"Error spring type in CMassSpringSystme GetSpringCoef"<<std::endl;
+        return -1.0;
+    }
+}
+double CMassSpringSystem::GetDamperCoef(const CSpring::enType_t a_cSpringType)
+{
+    if(a_cSpringType == CSpring::Type_nStruct)
+    {
+        return m_dDamperCoefStruct;
+    }
+    else if(a_cSpringType == CSpring::Type_nShear)
+    {
+        return m_dDamperCoefShear;
+    }
+    else if(a_cSpringType == CSpring::Type_nBending)
+    {
+        return m_dDamperCoefBending;
+    }
+    else
+    {
+        std::cout<<"Error spring type in CMassSpringSystme GetDamperCoef"<<std::endl;
+        return -1.0;
+    }
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Simulation Part
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+bool CMassSpringSystem::CheckStable()
+{
+    double threshold = 1e6;
+    for(int pIdx = 0; pIdx < m_GoalNet.ParticleNum(); pIdx++)
+    {
+        Vector3d particleVel = m_GoalNet.GetParticle(pIdx).GetVelocity();
+        if (particleVel.Magnitude() > threshold)
+        {
+            return false;
+        }  
+    }
+    return true;
+}
+void CMassSpringSystem::SimulationOneTimeStep()
+{
+    if(m_bSimulation)
+    {
+        Integrate();
+    }
+    
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Initialization
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void CMassSpringSystem::CreateBall()
+{
+    // randomly assign initial velocity and position to a ball
+    Ball newBall;
+    Vector3d randomOffset((double)(rand() % 5 + 5.0), (double)(rand() % 5), (double)(rand() % 5));
+    Vector3d randomVelOffset(0.0, 0.0, (double)(rand() % 3));
+    Vector3d initBallPos = m_GoalNet.GetInitPos() + randomOffset;
+    Vector3d initBallVel = (m_GoalNet.GetInitPos() + randomVelOffset - initBallPos)*7.0;
+    //Vector3d initBallVel = Vector3d::ZERO;
+    newBall.SetPosition(initBallPos);
+    newBall.SetVelocity(initBallVel);
+    m_Balls.push_back(newBall);
+}
+
+int CMassSpringSystem::BallNum()
+{
+    return m_Balls.size();
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Compute Force
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void CMassSpringSystem::ResetAllForce()
+{
+    for (int pIdx = 0; pIdx < m_GoalNet.ParticleNum(); ++pIdx)
+    {
+        m_GoalNet.GetParticle(pIdx).SetAcceleration(Vector3d::ZERO);
+    }
+
+    for (int ballIdx = 0; ballIdx < BallNum(); ++ballIdx)
+    {
+        m_Balls[ballIdx].SetAcceleration(Vector3d::ZERO);
+    }
+}
+
+void CMassSpringSystem::ComputeAllForce()
+{
+    ComputeParticleForce();
+    ComputeBallForce();
+}
+
+void CMassSpringSystem::ComputeParticleForce()
+{
+    m_GoalNet.AddForceField(m_ForceField);
+    m_GoalNet.ComputeInternalForce();
+}
+
+void CMassSpringSystem::ComputeBallForce()
+{
+    for (int ballIdx = 0; ballIdx < BallNum(); ++ballIdx)
+    {
+        m_Balls[ballIdx].AddForce(m_ForceField * m_Balls[ballIdx].GetMass());
+    }
+}
+
+void CMassSpringSystem::HandleCollision()
+{
+    ParticlePlaneCollision();
+    BallPlaneCollision();
+    BallToBallCollision();
+    BallParticleCollision();
+}
+
+void CMassSpringSystem::ParticlePlaneCollision()
+{
+    //TO DO 
+}
+
+void CMassSpringSystem::BallPlaneCollision()
+{
+    //TO DO
+}
+
+void CMassSpringSystem::BallToBallCollision()
+{
+    //TO DO
+}
+
+void CMassSpringSystem::BallParticleCollision()
+{
+    //TO DO
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Integrator
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void CMassSpringSystem::Integrate()
+{
+    if(m_iIntegratorType == CMassSpringSystem::EXPLICIT_EULER)
+    {
+        ComputeAllForce();
+        HandleCollision();
+	    ExplicitEuler();
+        ResetAllForce();
+    }
+    else if(m_iIntegratorType == CMassSpringSystem::RUNGE_KUTTA)
+    {
+        RungeKutta();
+    }
+    else
+    {
+        std::cout<<"Error integrator type, use explicit Euler instead!!"<<std::endl;
+        ComputeAllForce();
+        HandleCollision();
+        ExplicitEuler();
+        ResetAllForce();
+    }
+}
+
+void CMassSpringSystem::ExplicitEuler()
+{
+    //TO DO
+}
+
+void CMassSpringSystem::RungeKutta()
+{
+    //TO DO
+}
